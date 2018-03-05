43cd1b2fcdec188b9152eeb4f5824c180193f667
nalbam <me@nalbam.com>
2018-03-06 01:58:44 +0900

    c

--- domain/variables.tf
+++ domain/variables.tf
@@ -2,10 +2,14 @@ variable region {
   default = "us-east-1"
 }
 
+variable zone_id {
+  default = ""
+}
+
 variable domain_name {
-  //default = ""
+  default = ""
 }
 
 variable certificate_arn {
-  //default = ""
+  default = ""
 }
