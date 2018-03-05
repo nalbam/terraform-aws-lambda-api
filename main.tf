6499c269ac47c1bbf6a20da250434259a686e40e
nalbam <me@nalbam.com>
2018-03-06 02:22:44 +0900

    base_path_mapping

--- main.tf
+++ main.tf
@@ -71,7 +71,7 @@ resource "aws_api_gateway_domain_name" "default" {
   certificate_arn = "${var.certificate_arn}"
 }
 
-resource "aws_api_gateway_base_path_mapping" "test" {
+resource "aws_api_gateway_base_path_mapping" "default" {
   api_id = "${aws_api_gateway_rest_api.default.id}"
   stage_name = "${aws_api_gateway_deployment.default.stage_name}"
   domain_name = "${aws_api_gateway_domain_name.default.domain_name}"
