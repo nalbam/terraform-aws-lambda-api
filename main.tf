ff606709d94f456cd282e9ad67763d39f9ec5c56
nalbam <me@nalbam.com>
2018-07-16 14:02:08 +0900

    tf lint

--- main.tf
+++ main.tf
@@ -4,17 +4,17 @@ module "lambda" {
   source = "git::https://github.com/nalbam/terraform-aws-lambda.git"
   region = "${var.region}"
 
-  name = "${var.name}"
-  stage = "${var.stage}"
+  name        = "${var.name}"
+  stage       = "${var.stage}"
   description = "${var.description}"
-  runtime = "${var.runtime}"
-  handler = "${var.handler}"
+  runtime     = "${var.runtime}"
+  handler     = "${var.handler}"
   memory_size = "${var.memory_size}"
-  timeout = "${var.timeout}"
-  s3_bucket = "${var.s3_bucket}"
-  s3_source = "${var.s3_source}"
-  s3_key = "${var.s3_key}"
-  env_vars = "${var.env_vars}"
+  timeout     = "${var.timeout}"
+  s3_bucket   = "${var.s3_bucket}"
+  s3_source   = "${var.s3_source}"
+  s3_key      = "${var.s3_key}"
+  env_vars    = "${var.env_vars}"
 }
 
 resource "aws_api_gateway_rest_api" "default" {
@@ -23,8 +23,8 @@ resource "aws_api_gateway_rest_api" "default" {
 
 resource "aws_api_gateway_resource" "default" {
   rest_api_id = "${aws_api_gateway_rest_api.default.id}"
-  parent_id = "${aws_api_gateway_rest_api.default.root_resource_id}"
-  path_part = "${var.path_part}"
+  parent_id   = "${aws_api_gateway_rest_api.default.root_resource_id}"
+  path_part   = "${var.path_part}"
 }
 
 //resource "aws_cognito_user_pool" "default" {
@@ -46,47 +46,47 @@ resource "aws_api_gateway_resource" "default" {
 resource "aws_api_gateway_method" "default" {
   count = "${length(var.http_methods)}"
 
-  rest_api_id = "${aws_api_gateway_rest_api.default.id}"
-  resource_id = "${aws_api_gateway_resource.default.id}"
-  http_method = "${element(var.http_methods, count.index)}"
+  rest_api_id   = "${aws_api_gateway_rest_api.default.id}"
+  resource_id   = "${aws_api_gateway_resource.default.id}"
+  http_method   = "${element(var.http_methods, count.index)}"
   authorization = "NONE"
 
   depends_on = [
-    "aws_api_gateway_resource.default"
+    "aws_api_gateway_resource.default",
   ]
 }
 
 resource "aws_api_gateway_integration" "default" {
   count = "${length(var.http_methods)}"
 
-  type = "AWS_PROXY"
+  type        = "AWS_PROXY"
   rest_api_id = "${aws_api_gateway_rest_api.default.id}"
   resource_id = "${aws_api_gateway_resource.default.id}"
   http_method = "${element(var.http_methods, count.index)}"
-  uri = "${module.lambda.invoke_arn}"
+  uri         = "${module.lambda.invoke_arn}"
 
   # AWS lambdas can only be invoked with the POST method
   integration_http_method = "POST"
 
   depends_on = [
-    "aws_api_gateway_method.default"
+    "aws_api_gateway_method.default",
   ]
 }
 
 resource "aws_api_gateway_deployment" "default" {
   rest_api_id = "${aws_api_gateway_rest_api.default.id}"
-  stage_name = "${var.stage}"
+  stage_name  = "${var.stage}"
 
   depends_on = [
-    "aws_api_gateway_integration.default"
+    "aws_api_gateway_integration.default",
   ]
 }
 
 resource "aws_lambda_permission" "default" {
-  action = "lambda:InvokeFunction"
+  action        = "lambda:InvokeFunction"
   function_name = "${module.lambda.arn}"
-  principal = "apigateway.amazonaws.com"
-  statement_id = "AllowExecutionFromAPIGateway"
+  principal     = "apigateway.amazonaws.com"
+  statement_id  = "AllowExecutionFromAPIGateway"
 
   # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
   //source_arn = "${aws_api_gateway_deployment.default.execution_arn}/${aws_api_gateway_method.default_get_req.http_method}${aws_api_gateway_resource.default.path}"
@@ -96,13 +96,13 @@ resource "aws_lambda_permission" "default" {
 
 # https://github.com/terraform-providers/terraform-provider-aws/issues/2195
 resource "aws_api_gateway_domain_name" "default" {
-  domain_name = "${var.domain_name}"
+  domain_name     = "${var.domain_name}"
   certificate_arn = "${var.certificate_arn}"
 }
 
 resource "aws_api_gateway_base_path_mapping" "default" {
-  api_id = "${aws_api_gateway_rest_api.default.id}"
-  stage_name = "${aws_api_gateway_deployment.default.stage_name}"
+  api_id      = "${aws_api_gateway_rest_api.default.id}"
+  stage_name  = "${aws_api_gateway_deployment.default.stage_name}"
   domain_name = "${aws_api_gateway_domain_name.default.domain_name}"
 }
 
@@ -113,8 +113,8 @@ resource "aws_route53_record" "default" {
   type = "A"
 
   alias {
-    name = "${aws_api_gateway_domain_name.default.cloudfront_domain_name}"
-    zone_id = "${aws_api_gateway_domain_name.default.cloudfront_zone_id}"
+    name                   = "${aws_api_gateway_domain_name.default.cloudfront_domain_name}"
+    zone_id                = "${aws_api_gateway_domain_name.default.cloudfront_zone_id}"
     evaluate_target_health = "false"
   }
 }
