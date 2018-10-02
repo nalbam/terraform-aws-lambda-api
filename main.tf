285e1cc80c816c8a199c524fa6cfab7a616f8dbc
nalbam <me@nalbam.com>
2018-10-02 11:02:58 +0900

    domain

--- main.tf
+++ main.tf
@@ -77,28 +77,3 @@ resource "aws_lambda_permission" "default" {
   //source_arn = "arn:aws:execute-api:${var.region}:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.default.id}/*/${aws_api_gateway_method.default.http_method}${aws_api_gateway_resource.default.path}"
   source_arn = "arn:aws:execute-api:${var.region}:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.default.id}/*/*"
 }
-
-# https://github.com/terraform-providers/terraform-provider-aws/issues/2195
-resource "aws_api_gateway_domain_name" "default" {
-  domain_name     = "${var.domain_name}"
-  certificate_arn = "${var.certificate_arn}"
-}
-
-resource "aws_api_gateway_base_path_mapping" "default" {
-  api_id      = "${aws_api_gateway_rest_api.default.id}"
-  stage_name  = "${aws_api_gateway_deployment.default.stage_name}"
-  domain_name = "${aws_api_gateway_domain_name.default.domain_name}"
-}
-
-resource "aws_route53_record" "default" {
-  zone_id = "${var.zone_id}"
-
-  name = "${var.domain_name}"
-  type = "A"
-
-  alias {
-    name                   = "${aws_api_gateway_domain_name.default.cloudfront_domain_name}"
-    zone_id                = "${aws_api_gateway_domain_name.default.cloudfront_zone_id}"
-    evaluate_target_health = "false"
-  }
-}
